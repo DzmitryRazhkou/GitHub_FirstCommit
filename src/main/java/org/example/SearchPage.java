81ffe9383ca05e238f0377ae39f44711e6a2a5b1
dzmitryrazhkou <dzmitryrazhkou@gmail.com>
2022-08-27 14:18:14 -0700

    revised

--- src/main/java/org/example/SearchPage.java
+++ src/main/java/org/example/SearchPage.java
@@ -3,5 +3,7 @@ package org.example;
 public class SearchPage {
     public void SearchPage() {
         System.out.println("Yeah");
+        System.out.println("Ops");
+        System.out.println("Hey faggots!!!");
     }
 }
